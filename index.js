bf1d4dc12774287e5653db36e2d467742c5a192c
AshleyThomasDevelopment <ashleythomasdevelopment@gmail.com>
2023-10-25 03:50:21 -0500

    WIP Creates initial build
    User can create a new file in specified directory.
    
    User can pick an existing file from component folder and rename the
    react component inside the file itself.

--- index.js
+++ index.js
@@ -42,23 +42,20 @@ program
 
 const questions = [
     {
-        type: 'input',
-        name: 'name',
-        message: 'What is your name?'
-    },
-    {
         type: 'list',
         name: 'create',
         message: 'Create a file?',
         choices: ['Yes', 'No']
     },
-    {
-        type: 'list',
-        name: 'src_directory',
-        message: 'In the /src directory?',
-        choices: ['Yes', 'No']
-    },
 ];
+
+const srcDirPrompt = [{
+    type: 'list',
+    name: 'src_directory',
+    message: 'In the /src directory?',
+    choices: ['Yes', 'No']
+}]
+
 
 const whatDirPrompt = [
     {
@@ -76,66 +73,173 @@ const whatFilenamePrompt = [
     },
 ]
 
+const pickFilePrompt = [
+    {
+        type: 'list',
+        name: 'pickFile',
+        message: 'Pick an existing file?',
+        choices: ['Yes', 'No']
+    },
+]
+
+const renameFilePrompt = [
+    {
+        type: 'list',
+        name: 'rename',
+        message: 'Rename the component?',
+        choices: ['Yes', 'No']
+    },
+]
+
+const whatComponentPrompt = [
+    {
+        type: 'input',
+        name: 'what_compname',
+        message: 'What is the component to be called?'
+    },
+]
 
 
-const relativeDirectory = 'c:/Users/rideo/Dropbox/Code/projects/cli/initial-test'
+
+
+
+const relativeDirectory = `c:/Users/rideo/Dropbox/Code/projects/cli/initial-test`
+const componentDirectory = `${relativeDirectory}/library/components`
+
 
 inquirer
     .prompt(questions)
     .then(answers => {
-        console.log(`Hello, ${answers.name}!`);
         if (answers.create === 'Yes') {
 
-            if (answers.src_directory === 'Yes') {
+            inquirer
+                .prompt(srcDirPrompt)
+                .then(answers => {
 
-                inquirer
-                    .prompt(whatFilenamePrompt)
-                    .then(answers => {
-                        
-                        fs.writeFile(`${relativeDirectory}/${answers.what_filename}.txt`, 'Hello, World!', (err) => {
-                            if (err) {
-                                console.error('Error creating the file:', err);
-                            } else {
-                                console.log('File created successfully!');
-                            }
-                        });
-                    })
-                    .catch(error => {
-                        console.error('Error occurred:', error);
-                    });
-            } else {
+                    if (answers.src_directory === 'Yes') {
 
+                        inquirer
+                            .prompt(whatFilenamePrompt)
+                            .then(answers => {
 
-                inquirer
-                    .prompt(whatDirPrompt)
-                    .then(answers => {
-
-                        const what_dir = answers.what_dir
-
-                        inquirer.prompt(whatFilenamePrompt)
-                        .then(answers => {
-
-                            fs.mkdir(`${relativeDirectory}/${what_dir}`, {recursive: true}, (err) => {
-                                if (err) {
-                                    console.error('Error creating directory:', err);
-                                  } else {
-                                    console.log('Directory created successfully!');
-                                    fs.writeFile(`${relativeDirectory}/${what_dir}/${answers.what_filename}`, `console.log('Hello, World!')`, (err) => {
-                                        if (err) {
-                                            console.error('Error creating the file:', err);
-                                        } else {
-                                            console.log('File created successfully!');
-                                        }
-                                    });
-                                  }
+                                fs.writeFile(`${relativeDirectory}/${answers.what_filename}.txt`, 'Hello, World!', (err) => {
+                                    if (err) {
+                                        console.error('Error creating the file:', err);
+                                    } else {
+                                        console.log('File created successfully!');
+                                    }
+                                });
                             })
+                            .catch(error => {
+                                console.error('Error occurred:', error);
+                            });
+                    } else {
 
-                        })
-                    })
-                    .catch(error => {
-                        console.error('Error occurred in what_dir block:', error);
-                    });
-            }
+
+                        inquirer
+                            .prompt(whatDirPrompt)
+                            .then(answers => {
+
+                                const what_dir = answers.what_dir
+
+                                inquirer.prompt(whatFilenamePrompt)
+                                    .then(answers => {
+
+                                        fs.mkdir(`${relativeDirectory}/${what_dir}`, { recursive: true }, (err) => {
+                                            if (err) {
+                                                console.error('Error creating directory:', err);
+                                            } else {
+                                                console.log('Directory created successfully!');
+                                                fs.writeFile(`${relativeDirectory}/${what_dir}/${answers.what_filename}`, `console.log('Hello, World!')`, (err) => {
+                                                    if (err) {
+                                                        console.error('Error creating the file:', err);
+                                                    } else {
+                                                        console.log('File created successfully!');
+                                                    }
+                                                });
+                                            }
+                                        })
+
+                                    })
+                            })
+                            .catch(error => {
+                                console.error('Error occurred in what_dir block:', error);
+                            });
+                    }
+                })
+        } else {
+            inquirer
+                .prompt(pickFilePrompt)
+                .then(answers => {
+                    if (answers.pickFile === 'Yes') {
+
+                        // fs.readdir(componentDirectory, (err, files) => {
+                        //     if (err) {
+                        //         console.error('Error reading directory:', err);
+                        //     } else {
+                        //         console.log('Files in the directory:', files);
+                        //     }
+                        // });
+
+
+                        // Function to generate dynamic choices from files in a directory
+                        async function generateDynamicChoices() {
+                            // const directoryPath = './path/to/your/directory'; // Replace with the path of your directory
+                            try {
+                                const files = await fs.promises.readdir(componentDirectory);
+                                return files;
+                            } catch (error) {
+                                console.error('Error reading directory:', error);
+                                return [];
+                            }
+                        }
+
+                        // Prompt definition
+                        const prompt = {
+                            type: 'list',
+                            name: 'selectedFile',
+                            message: 'Select a file from the directory:',
+                            choices: generateDynamicChoices // Use the function as the choices property
+                        };
+
+                        // Ask the user to select a file from the directory
+                        inquirer.prompt(prompt).then(answers => {
+                            console.log('Selected file:', answers.selectedFile);
+                            const selectedFilePath = `${componentDirectory}/${answers.selectedFile}`
+                            try {
+                                const data = fs.readFileSync(selectedFilePath, 'utf8');
+
+                                inquirer
+                                    .prompt(renameFilePrompt)
+                                    .then(answers => {
+                                        if (answers.rename === 'Yes') {
+                                            inquirer
+                                                .prompt(whatComponentPrompt)
+                                                .then(answers => {
+
+                                                    const newContent = data.replace(/!!NAME!!/g, answers.what_compname);
+
+                                                    fs.writeFile(selectedFilePath, newContent, (err) => {
+                                                        if (err) {
+                                                            console.error('Error writing to file:', err);
+                                                        } else {
+                                                            console.log('File content changed successfully.');
+                                                        }
+                                                    });
+
+
+
+                                                })
+                                        }
+                                    })
+                                // console.log('File content:', data);
+                            } catch (err) {
+                                console.error('Error reading file:', err);
+                            }
+
+                        });
+                    }
+                })
         }
     })
     .catch(error => {
