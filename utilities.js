2ffad74f4b250c7bc88f9af2fb606676935888d3
AshleyThomasDevelopment <ashleythomasdevelopment@gmail.com>
2023-11-07 20:32:53 -0800

    Adds temp stickyItems code

--- utilities.js
+++ utilities.js
@@ -49,3 +49,43 @@ export function styled(input, style) {
             break;
     }
 }
+
+
+
+
+
+const inquirer = require('inquirer');
+
+const items = [
+  'Fixed Item 1',
+  'Fixed Item 2',
+  'Item 3',
+  'Item 4',
+  'Item 5',
+  // Add more items as needed
+];
+
+const pageSize = 3; // Number of visible items excluding fixed items
+let currentIndex = 0;
+
+function renderList() {
+  const visibleItems = items.slice(currentIndex, currentIndex + pageSize);
+  const fixedItems = items.slice(0, currentIndex);
+  const formattedItems = [...fixedItems, ...visibleItems];
+
+  inquirer
+    .prompt([
+      {
+        type: 'list',
+        name: 'selectedItem',
+        message: 'Scrolling List with Fixed Items',
+        choices: formattedItems,
+      },
+    ])
+    .then(answer => {
+      // Handle user input here
+      console.log(`You selected: ${answer.selectedItem}`);
+    });
+}
+
+renderList();
